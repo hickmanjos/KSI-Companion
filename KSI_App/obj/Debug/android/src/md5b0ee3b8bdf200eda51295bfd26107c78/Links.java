96b39825a2f8be68cea708f3408cc6a40c2496a4
hickmanjos <hickmanjos@gmail.com>
2015-05-06 11:33:17 -0500

    v0.1
    Began filling out basic functionality.

---
+++ KSI_App/obj/Debug/android/src/md5b0ee3b8bdf200eda51295bfd26107c78/Links.java
@@ -0,0 +1,46 @@
+package md5b0ee3b8bdf200eda51295bfd26107c78;
+
+
+public class Links
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("KSI_App.Links, KSI_App, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Links.class, __md_methods);
+	}
+
+
+	public Links () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == Links.class)
+			mono.android.TypeManager.Activate ("KSI_App.Links, KSI_App, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
