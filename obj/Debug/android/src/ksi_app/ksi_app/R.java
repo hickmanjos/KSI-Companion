15720c90513da9424c93ea423bb9569f402a0e9e
hickmanjos <hickmanjos@gmail.com>
2015-05-13 18:12:52 -0500

    Added Hall of Fame

--- obj/Debug/android/src/ksi_app/ksi_app/R.java
+++ obj/Debug/android/src/ksi_app/ksi_app/R.java
@@ -11,9 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int ksi=0x7f020001;
-        public static final int monoandroidsplash=0x7f020002;
+        public static final int halloffame=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int ksi=0x7f020002;
+        public static final int monoandroidsplash=0x7f020003;
     }
     public static final class id {
         public static final int MyButton=0x7f060007;
